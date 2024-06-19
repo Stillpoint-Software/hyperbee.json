d4b934c71359fb4c2c578ca27cf8514d76311a2d
Brenton Farmer <brent.farmer@wagglebee.net>
2024-06-19 16:04:56 -0700

    Changed names for consistency

--- test/Hyperbee.Json.Tests/Tokenizer/JsonPathQueryTokenizerTests.cs
+++ test/Hyperbee.Json.Tests/Tokenizer/JsonPathQueryTokenizerTests.cs
@@ -7,52 +7,53 @@ namespace Hyperbee.Json.Tests.Tokenizer;
 public class JsonPathQueryTokenizerTests
 {
     [DataTestMethod]
-    [DataRow( "$", "{$|k}" )]
-    [DataRow( "$.two.some", "{$|k};{two|k};{some|k}" )]
-    [DataRow( "$.thing[1:2:3]", "{$|k};{thing|k};{1:2:3|s}" )]
-    [DataRow( "$..thing[?(@.x == 1)]", "{$|k};{..|s};{thing|k};{?(@.x == 1)|s}" )]
-    [DataRow( "$['two.some']", "{$|k};{two.some|k}" )]
-    [DataRow( "$.two.some.thing['this.or.that']", "{$|k};{two|k};{some|k};{thing|k};{this.or.that|k}" )]
-    [DataRow( "$.store.book[*].author", "{$|k};{store|k};{book|k};{*|s};{author|k}" )]
-    [DataRow( "@..author", "{@|k};{..|s};{author|k}" )]
-    [DataRow( "$.store.*", "{$|k};{store|k};{*|s}" )]
-    [DataRow( "$.store..price", "{$|k};{store|k};{..|s};{price|k}" )]
-    [DataRow( "$..book[2]", "{$|k};{..|s};{book|k};{2|k}" )]
-    [DataRow( "$..book[-1:]", "{$|k};{..|s};{book|k};{-1:|s}" )]
-    [DataRow( "$..book[:2]", "{$|k};{..|s};{book|k};{:2|s}" )]
-    [DataRow( "$..book[0,1]", "{$|k};{..|s};{book|k};{1,0|s}" )]
-    [DataRow( "$.store.book[0,1]", "{$|k};{store|k};{book|k};{1,0|s}" )]
-    [DataRow( "$..book['category','author']", "{$|k};{..|s};{book|k};{author,category|s}" )]
-    [DataRow( "$..book[?(@.isbn)]", "{$|k};{..|s};{book|k};{?(@.isbn)|s}" )]
-    [DataRow( "$..book[?@.isbn]", "{$|k};{..|s};{book|k};{?@.isbn|s}" )]
-    [DataRow( "$..book[?(@.price<10)]", "{$|k};{..|s};{book|k};{?(@.price<10)|s}" )]
-    [DataRow( "$..book[?@.price<10]", "{$|k};{..|s};{book|k};{?@.price<10|s}" )]
-    [DataRow( "$..*", "{$|k};{..|s};{*|s}" )]
-    [DataRow( """$.store.book[?(@path !== "$['store']['book'][0]")]""", """{$|k};{store|k};{book|k};{?(@path !== "$['store']['book'][0]")|s}""" )]
-    [DataRow( """$..book[?(@.price == 8.99 && @.category == "fiction")]""", """{$|k};{..|s};{book|k};{?(@.price == 8.99 && @.category == "fiction")|s}""" )]
+    [DataRow( "$", "{$|s}" )]
+    [DataRow( "$.two.some", "{$|s};{two|s};{some|s}" )]
+    [DataRow( "$.thing[1:2:3]", "{$|s};{thing|s};{1:2:3|g}" )]
+    [DataRow( "$..thing[?(@.x == 1)]", "{$|s};{..|g};{thing|s};{?(@.x == 1)|g}" )]
+    [DataRow( "$['two.some']", "{$|s};{two.some|s}" )]
+    [DataRow( "$.two.some.thing['this.or.that']", "{$|s};{two|s};{some|s};{thing|s};{this.or.that|s}" )]
+    [DataRow( "$.store.book[*].author", "{$|s};{store|s};{book|s};{*|g};{author|s}" )]
+    [DataRow( "@..author", "{@|s};{..|g};{author|s}" )]
+    [DataRow( "$.store.*", "{$|s};{store|s};{*|g}" )]
+    [DataRow( "$.store..price", "{$|s};{store|s};{..|g};{price|s}" )]
+    [DataRow( "$..book[2]", "{$|s};{..|g};{book|s};{2|s}" )]
+    [DataRow( "$..book[-1:]", "{$|s};{..|g};{book|s};{-1:|g}" )]
+    [DataRow( "$..book[:2]", "{$|s};{..|g};{book|s};{:2|g}" )]
+    [DataRow( "$..book[0,1]", "{$|s};{..|g};{book|s};{1,0|g}" )]
+    [DataRow( "$.store.book[0,1]", "{$|s};{store|s};{book|s};{1,0|g}" )]
+    [DataRow( "$..book['category','author']", "{$|s};{..|g};{book|s};{author,category|g}" )]
+    [DataRow( "$..book[?(@.isbn)]", "{$|s};{..|g};{book|s};{?(@.isbn)|g}" )]
+    [DataRow( "$..book[?@.isbn]", "{$|s};{..|g};{book|s};{?@.isbn|g}" )]
+    [DataRow( "$..book[?(@.price<10)]", "{$|s};{..|g};{book|s};{?(@.price<10)|g}" )]
+    [DataRow( "$..book[?@.price<10]", "{$|s};{..|g};{book|s};{?@.price<10|g}" )]
+    [DataRow( "$..*", "{$|s};{..|g};{*|g}" )]
+    [DataRow( """$.store.book[?(@path !== "$['store']['book'][0]")]""", """{$|s};{store|s};{book|s};{?(@path !== "$['store']['book'][0]")|g}""" )]
+    [DataRow( """$..book[?(@.price == 8.99 && @.category == "fiction")]""", """{$|s};{..|g};{book|s};{?(@.price == 8.99 && @.category == "fiction")|g}""" )]
     public void Should_tokenize_json_path( string jsonPath, string expected )
     {
+        // act
+        var tokens = JsonPathQueryParser.Parse( jsonPath );
+
         // arrange
+        var result = TokensToString( tokens );
+
+        // assert
+        Assert.AreEqual( expected, result );
+        return;
+
         static string TokensToString( JsonPathSegment segment )
         {
             return string.Join( ';', segment.AsEnumerable().Select( TokenToString ) );
 
             static string TokenToString( JsonPathSegment segment )
             {
-                var (keySelector, selectors) = segment;
-                var selectorType = keySelector ? "k" : "s";
+                var (singular, selectors) = segment;
+                var selectorType = singular ? "s" : "g"; // s:singular, g:group
                 var selectorsString = string.Join( ',', selectors.Select( x => x.Value ) );
 
                 return $"{{{selectorsString}|{selectorType}}}";
             }
         }
-
-        // act
-        var tokens = JsonPathQueryParser.Parse( jsonPath );
-
-        // assert
-        var result = TokensToString( tokens );
-
-        Assert.AreEqual( expected, result );
     }
 }

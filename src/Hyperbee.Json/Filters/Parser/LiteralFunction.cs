0505a8ff6e4eb52a8cd31c93b2904251a92caaab
Brenton Farmer <brent.farmer@wagglebee.net>
2024-06-19 12:30:12 -0700

    removed comment block

--- src/Hyperbee.Json/Filters/Parser/LiteralFunction.cs
+++ src/Hyperbee.Json/Filters/Parser/LiteralFunction.cs
@@ -35,38 +35,6 @@ public class LiteralFunction : FilterFunction
         }
     }
 
-    //protected override Expression GetExpressionImpl( ReadOnlySpan<char> data, ReadOnlySpan<char> item, ref int start, ref int from )
-    //{
-    //    // strings double or single
-    //    if ( FilterTokenizerRegex.RegexQuotedDouble().IsMatch( item ) )
-    //        return Expression.Constant( TrimQuotes( item ).ToString() );
-    //    if ( FilterTokenizerRegex.RegexQuoted().IsMatch( item ) )
-    //        return Expression.Constant( TrimQuotes( item ).ToString() );
-
-    //    // known literals (true, false, null)
-    //    if ( item.Equals( KnownLiterals.TrueSpan, StringComparison.OrdinalIgnoreCase ) )
-    //        return Expression.Constant( true );
-    //    if ( item.Equals( KnownLiterals.FalseSpan, StringComparison.OrdinalIgnoreCase ) )
-    //        return Expression.Constant( false );
-    //    if ( item.Equals( KnownLiterals.NullSpan, StringComparison.OrdinalIgnoreCase ) )
-    //        return Expression.Constant( null );
-
-    //    // numbers
-    //    // TODO: Currently assuming all numbers are floats since we don't know what's in the data or the other side of the operator yet.
-    //    return Expression.Constant( float.Parse( item ) );
-
-    //    static ReadOnlySpan<char> TrimQuotes( ReadOnlySpan<char> input )
-    //    {
-    //        if ( input.Length < 2 )
-    //            return input;
-
-    //        if ( input[0] == '\'' && input[^1] == '\'' || input[0] == '\"' && input[^1] == '\"' )
-    //            return input[1..^1];
-
-    //        return input;
-    //    }
-    //}
-
     internal ref struct KnownLiterals
     {
         internal static ReadOnlySpan<char> TrueSpan => "true".AsSpan();
